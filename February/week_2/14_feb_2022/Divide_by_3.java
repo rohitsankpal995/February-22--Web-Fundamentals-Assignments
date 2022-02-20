2d5ecdd6b3ba1c98e57b67de783ea017cb441232
rohitsankpal995 <98374296+rohitsankpal995@users.noreply.github.com>
2022-02-20 19:13:41 +0530

    Add files via upload

---
+++ February/week_2/14_feb_2022/Divide_by_3.java
@@ -0,0 +1,15 @@
+package Feb_14_2022;
+
+public class Divide_by_3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("\nDivided by 3: ");
+		for (int i = 1; i < 100; i++) {
+			if (i % 3 == 0)
+				System.out.print(i + ", ");
+		}
+	}
+
+}
---
+++ February/week_2/14_feb_2022/WAP_10_to_1.java
@@ -0,0 +1,18 @@
+package Feb_14_2022;
+
+public class WAP_10_to_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// loop counter initialisation
+		int i = 10;
+		
+		while (i > 0) {
+			System.out.println(i);
+			i--;
+		}
+
+	}
+
+}
