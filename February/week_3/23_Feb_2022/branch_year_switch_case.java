ddd0aa5ca9567396cb88f3b6cbdd0560b20bc2f3
rohitsankpal995 <98374296+rohitsankpal995@users.noreply.github.com>
2022-02-27 17:35:34 +0530

    Add files via upload

---
+++ February/week_3/23_Feb_2022/branch_year_switch_case.java
@@ -0,0 +1,10 @@
+package feb_23_2022;
+
+public class branch_year_switch_case {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ February/week_3/23_Feb_2022/even_no_1_to_10.java
@@ -0,0 +1,28 @@
+package feb_23_2022;
+import java.util.*;
+
+public class even_no_1_to_10 {
+
+	public static void main(String[] args) {
+	int n = 10; 
+	int i = 1;
+	for (i=1;i<=n;i++) {
+			if(i>n) {
+				return;
+			}
+			   
+			if(i%2==0)   
+			{   
+			 
+			System.out.println(i);
+			}
+	
+				
+			
+		}
+		   
+			
+
+	}
+
+}
